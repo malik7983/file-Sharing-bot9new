2c4a8e145bd53d1b6a126a44171657c8f093f5af
savezmalik7960 <101207459+savezmalik7960@users.noreply.github.com>
2022-04-17 05:08:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/malik7983/File-Sharing-Bot9.git /File-Sharing-Bot9
+  git clone https://github.com/savezmalik7960/File-Sharing-Bot9new.git /File-Sharing-Bot9new
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /File-Sharing-Bot9
+  git clone $UPSTREAM_REPO /File-Sharing-Bot9new
 fi
-cd /File-Sharing-Bot9
+cd /File-Sharing-Bot9new
 pip3 install -U -r requirements.txt
 echo "Starting bot....🔥"
 python3 bot.py
