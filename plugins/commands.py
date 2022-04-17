bd092ca68632b2d937bfdc84e674535c9d2e8f31
savezmalik7960 <101207459+savezmalik7960@users.noreply.github.com>
2022-04-18 05:16:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,9 +43,9 @@ async def start(client, message):
             InlineKeyboardButton('CONTACT ME', url='https://t.me/sahid_malik'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('⚡️join this channel to Use me 💥', url='https://t.me/+ZeZNvt43B4o3ZmJl'),
-            InlineKeyboardButton('✅ Subscribe my YouTube channel  ✅', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A')
+            InlineKeyboardButton('⚡️join this channel to Use me 💥', url='https://t.me/+ZeZNvt43B4o3ZmJl')
             ],[
+            InlineKeyboardButton('✅ Subscribe my YouTube channel  ✅', url='https://youtube.com/channel/UCPaHDqWf3D3w2nxb8p3sr4A')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
